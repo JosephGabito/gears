dd2cac2f93d1663fb49c84fda427ba25a8ef941c
Joseph Gabito <codehaiku.io@gmail.com>
2017-01-18 18:38:51 +0800

    Added Shortcode Support for VC

---
+++ modules/shortcodes/vc/gears_login.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Gears Login Visual Composer
+ * Sample Shortcode: [gears_login]
+ *
+ * @since 4.1.1
+ */
+
+vc_map(
+	array(
+		"name" => __("Gears Login"),
+		"base" => "gears_login",
+		"class" => "",
+		"admin_label" => true,
+		"category" => __('Gears'),
+		"icon" => plugins_url('../../../assets/images/login.png', __FILE__),
+		'admin_enqueue_js' => array(),
+		'admin_enqueue_css' => array()
+	)
+);
+?>
