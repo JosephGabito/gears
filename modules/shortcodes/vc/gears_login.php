1d7477001c63013c0d2762843f0f2de680d24ffb
Joseph Gabito <codehaiku.io@gmail.com>
2017-04-26 15:07:55 +0800

    fixed conflict with WPDM

--- modules/shortcodes/vc/gears_login.php
+++ modules/shortcodes/vc/gears_login.php
@@ -16,7 +16,19 @@ vc_map(
 		"category" => __('Gears'),
 		"icon" => plugins_url('../../../assets/images/login.png', __FILE__),
 		'admin_enqueue_js' => array(),
-		'admin_enqueue_css' => array()
+		'admin_enqueue_css' => array(),
+		'params' => array(
+			array(
+				"type" => "textfield",
+				"holder" => "",
+				"class" => "",
+				"admin_label" => true,
+				"heading" => __("ID", "gears"),
+				"param_name" => "id",
+				"value" => "",
+				"description" => __("If you are hooking the form through JS, you can add a custom element ID here.", "gears")
+			),
+		)
 	)
 );
 ?>
