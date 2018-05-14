4907b1dea5a3dbfecc621fb685ea7d3cb5ee9131
jasperjardin <jasper.jardin1994@gmail.com>
2018-05-14 10:12:16 +0800

    Incremented the plugin version.

--- gears.php
+++ gears.php
@@ -3,7 +3,7 @@
  * Plugin Name: Gears
  * Plugin URI: http://themeforest.net/user/dunhakdis
  * Description: Gears empowers BuddyPress to have more features like cover photos, social login, shortcodes, and more!
- * Version: 4.1.5
+ * Version: 4.1.6
  * Author: Dunhakdis
  * Author URI: http://themeforest.net/user/dunhakdis
  * License: GPL2
