b517034ecaedfc56cb4ef8c98142a51a80e378ae
jasperjardin <jasper.jardin1994@gmail.com>
2018-05-14 13:46:09 +0800

    Checked if the BP_AVATAR_FULL_WIDTH constant is defined.

--- modules/shortcodes/vc/gears_bp_groups_carousel.php
+++ modules/shortcodes/vc/gears_bp_groups_carousel.php
@@ -6,88 +6,92 @@
  */
 
 // [gears_bp_groups_carousel type=’active’ max_item =’10’ max_slides=’7’ min_slides=’1’ item_width=’100’]
+$bp_avatar_full_width = 325;
+
 if ( defined( 'BP_AVATAR_FULL_WIDTH' ) ) {
-    vc_map(
-    	array(
-    		"name" => __("BP Groups Carousel"),
-    		"base" => "gears_bp_groups_carousel",
-    		"class" => "",
-    		"admin_label" => true,
-    		"category" => __('Gears'),
-    		"icon" => plugins_url('../../../assets/images/bp-groups-carousel.png', __FILE__),
-    		'admin_enqueue_js' => array(),
-    		'admin_enqueue_css' => array(),
-    		"params" => array(
-    				array(
-    					"type" => "dropdown",
-    					"holder" => "",
-    					"class" => "",
-    					"admin_label" => true,
-    					"heading" => __("Type", "gears"),
-    					"param_name" => "type",
-    					"value" => array(
-    							__('Active', "gears") => 'active',
-    							__('Newest', "gears") => 'newest',
-    							__('Popular', "gears") => 'popular',
-    							__('Alphabetical', "gears") => 'alphabetical',
-    							__('Most Forum Topics', "gears") => 'most-forum-topics',
-    							__('Most Forum Posts', "gears") => 'most-forum-posts',
-    							__('Random', "gears") => 'random'
-    						),
-    					"description" => __("The type of members you want to display.", "gears")
-    				),
-    				array(
-    					"type" => "textfield",
-    					"holder" => "",
-    					"class" => "",
-    					"admin_label" => true,
-    					"heading" => __("Max", "gears"),
-    					"param_name" => "max_item",
-    					"value" => 12,
-    					"description" => __("The number of members you want to display.", "gears")
-    				),
-    				array(
-    					"type" => "textfield",
-    					"holder" => "",
-    					"class" => "",
-    					"admin_label" => true,
-    					"heading" => __("Max Slides", "gears"),
-    					"param_name" => "max_slides",
-    					"value" => 7,
-    					"description" => __("The maximum number of item to show per slide.", "gears")
-    				),
-    				array(
-    					"type" => "textfield",
-    					"holder" => "",
-    					"class" => "",
-    					"admin_label" => true,
-    					"heading" => __("Min Slides", "gears"),
-    					"param_name" => "min_slides",
-    					"value" => 1,
-    					"description" => __("The minimum number of item to show per slide.", "gears")
-    				),
-    				array(
-    					"type" => "textfield",
-    					"holder" => "",
-    					"class" => "",
-    					"admin_label" => true,
-    					"heading" => __("Slide Margin", "gears"),
-    					"param_name" => "slide_margin",
-    					"value" => 0,
-    					"description" => __("The spacing for each item in the carousel.", "gears")
-    				),
-    				array(
-    					"type" => "textfield",
-    					"holder" => "",
-    					"class" => "",
-    					"admin_label" => true,
-    					"heading" => __("Item Width", "gears"),
-    					"param_name" => "item_width",
-    					"value" => '223',
-    					"description" => sprintf(__("The width of each item (ex. '223'). Recommended maximum width '%s'", "gears"),BP_AVATAR_FULL_WIDTH)
-    				)
-    			)
-    	)
-    );
+    $bp_avatar_full_width = BP_AVATAR_FULL_WIDTH;
 }
+
+vc_map(
+	array(
+		"name" => __("BP Groups Carousel"),
+		"base" => "gears_bp_groups_carousel",
+		"class" => "",
+		"admin_label" => true,
+		"category" => __('Gears'),
+		"icon" => plugins_url('../../../assets/images/bp-groups-carousel.png', __FILE__),
+		'admin_enqueue_js' => array(),
+		'admin_enqueue_css' => array(),
+		"params" => array(
+				array(
+					"type" => "dropdown",
+					"holder" => "",
+					"class" => "",
+					"admin_label" => true,
+					"heading" => __("Type", "gears"),
+					"param_name" => "type",
+					"value" => array(
+							__('Active', "gears") => 'active',
+							__('Newest', "gears") => 'newest',
+							__('Popular', "gears") => 'popular',
+							__('Alphabetical', "gears") => 'alphabetical',
+							__('Most Forum Topics', "gears") => 'most-forum-topics',
+							__('Most Forum Posts', "gears") => 'most-forum-posts',
+							__('Random', "gears") => 'random'
+						),
+					"description" => __("The type of members you want to display.", "gears")
+				),
+				array(
+					"type" => "textfield",
+					"holder" => "",
+					"class" => "",
+					"admin_label" => true,
+					"heading" => __("Max", "gears"),
+					"param_name" => "max_item",
+					"value" => 12,
+					"description" => __("The number of members you want to display.", "gears")
+				),
+				array(
+					"type" => "textfield",
+					"holder" => "",
+					"class" => "",
+					"admin_label" => true,
+					"heading" => __("Max Slides", "gears"),
+					"param_name" => "max_slides",
+					"value" => 7,
+					"description" => __("The maximum number of item to show per slide.", "gears")
+				),
+				array(
+					"type" => "textfield",
+					"holder" => "",
+					"class" => "",
+					"admin_label" => true,
+					"heading" => __("Min Slides", "gears"),
+					"param_name" => "min_slides",
+					"value" => 1,
+					"description" => __("The minimum number of item to show per slide.", "gears")
+				),
+				array(
+					"type" => "textfield",
+					"holder" => "",
+					"class" => "",
+					"admin_label" => true,
+					"heading" => __("Slide Margin", "gears"),
+					"param_name" => "slide_margin",
+					"value" => 0,
+					"description" => __("The spacing for each item in the carousel.", "gears")
+				),
+				array(
+					"type" => "textfield",
+					"holder" => "",
+					"class" => "",
+					"admin_label" => true,
+					"heading" => __("Item Width", "gears"),
+					"param_name" => "item_width",
+					"value" => '223',
+					"description" => sprintf(__("The width of each item (ex. '223'). Recommended maximum width '%s'", "gears"),$bp_avatar_full_width)
+				)
+			)
+	)
+);
 ?>
--- modules/shortcodes/vc/gears_bp_members_carousel.php
+++ modules/shortcodes/vc/gears_bp_members_carousel.php
@@ -5,93 +5,98 @@
  *
  * @since 1.0
  */
+
+$bp_avatar_full_width = 325;
+
 if ( defined( 'BP_AVATAR_FULL_WIDTH' ) ) {
-    vc_map(
-    	array(
-    		"name" => __("BP Members Carousel"),
-    		"base" => "gears_bp_members_carousel",
-    		"class" => "",
-    		"admin_label" => true,
-    		"category" => __('Gears'),
-    		"icon" => plugins_url('../../../assets/images/bp-members-carousel.png', __FILE__),
-    		'admin_enqueue_js' => array(),
-    		'admin_enqueue_css' => array(),
-    		"params" => array(
-    				array(
-    					"type" => "dropdown",
-    					"holder" => "",
-    					"class" => "",
-    					"admin_label" => true,
-    					"heading" => __("Type", "gears"),
-    					"admin_label" => true,
-    					"param_name" => "type",
-    					"value" => array(
-    							__('Active', "gears") => 'active',
-    							__('Newest', "gears") => 'newest',
-    							__('Popular', "gears") => 'popular',
-    							__('Online', "gears") => 'online',
-    							__('Alphabetical', "gears") => 'alphabetical',
-    							__('Random', "gears") => 'random'
-    						),
-    					"description" => __("The type of members you want to display.", "gears")
-    				),
-    				array(
-    					"type" => "textfield",
-    					"holder" => "",
-    					"class" => "",
-    					"admin_label" => true,
-    					"admin_label" => true,
-    					"heading" => __("Max", "gears"),
-    					"param_name" => "max_item",
-    					"value" => 10,
-    					"description" => __("The number of members you want to display.", "gears")
-    				),
-    				array(
-    					"type" => "textfield",
-    					"holder" => "",
-    					"class" => "",
-    					"admin_label" => true,
-    					"admin_label" => true,
-    					"heading" => __("Max Slide", "gears"),
-    					"param_name" => "max_slides",
-    					"value" => 7,
-    					"description" => __("Maximum number of item to display in slides.", "gears")
-    				),
-    				array(
-    					"type" => "textfield",
-    					"holder" => "",
-    					"class" => "",
-    					"admin_label" => true,
-    					"admin_label" => true,
-    					"heading" => __("Min Slide", "gears"),
-    					"param_name" => "min_slides",
-    					"value" => 1,
-    					"description" => __("Minimum number of item to display in slides. Applies to mobile.", "gears")
-    				),
-    				array(
-    					"type" => "textfield",
-    					"holder" => "",
-    					"class" => "",
-    					"admin_label" => true,
-    					"admin_label" => true,
-    					"heading" => __("Slide Margin", "gears"),
-    					"param_name" => "margin",
-    					"value" => 40,
-    					"description" => __("The spacing for each item in the carousel.", "gears")
-    				),
-    				array(
-    					"type" => "textfield",
-    					"holder" => "",
-    					"class" => "",
-    					"admin_label" => true,
-    					"admin_label" => true,
-    					"heading" => __("Item Width", "gears"),
-    					"param_name" => "item_width",
-    					"value" => 228,
-    					"description" => sprintf(__("The width of each item in the slide. Recommended maximum width is '%s'", "gears"), BP_AVATAR_FULL_WIDTH)
-    				)
-    			)
-    	)
-    );
+    $bp_avatar_full_width = BP_AVATAR_FULL_WIDTH;
 }
+
+vc_map(
+	array(
+		"name" => __("BP Members Carousel"),
+		"base" => "gears_bp_members_carousel",
+		"class" => "",
+		"admin_label" => true,
+		"category" => __('Gears'),
+		"icon" => plugins_url('../../../assets/images/bp-members-carousel.png', __FILE__),
+		'admin_enqueue_js' => array(),
+		'admin_enqueue_css' => array(),
+		"params" => array(
+				array(
+					"type" => "dropdown",
+					"holder" => "",
+					"class" => "",
+					"admin_label" => true,
+					"heading" => __("Type", "gears"),
+					"admin_label" => true,
+					"param_name" => "type",
+					"value" => array(
+							__('Active', "gears") => 'active',
+							__('Newest', "gears") => 'newest',
+							__('Popular', "gears") => 'popular',
+							__('Online', "gears") => 'online',
+							__('Alphabetical', "gears") => 'alphabetical',
+							__('Random', "gears") => 'random'
+						),
+					"description" => __("The type of members you want to display.", "gears")
+				),
+				array(
+					"type" => "textfield",
+					"holder" => "",
+					"class" => "",
+					"admin_label" => true,
+					"admin_label" => true,
+					"heading" => __("Max", "gears"),
+					"param_name" => "max_item",
+					"value" => 10,
+					"description" => __("The number of members you want to display.", "gears")
+				),
+				array(
+					"type" => "textfield",
+					"holder" => "",
+					"class" => "",
+					"admin_label" => true,
+					"admin_label" => true,
+					"heading" => __("Max Slide", "gears"),
+					"param_name" => "max_slides",
+					"value" => 7,
+					"description" => __("Maximum number of item to display in slides.", "gears")
+				),
+				array(
+					"type" => "textfield",
+					"holder" => "",
+					"class" => "",
+					"admin_label" => true,
+					"admin_label" => true,
+					"heading" => __("Min Slide", "gears"),
+					"param_name" => "min_slides",
+					"value" => 1,
+					"description" => __("Minimum number of item to display in slides. Applies to mobile.", "gears")
+				),
+				array(
+					"type" => "textfield",
+					"holder" => "",
+					"class" => "",
+					"admin_label" => true,
+					"admin_label" => true,
+					"heading" => __("Slide Margin", "gears"),
+					"param_name" => "margin",
+					"value" => 40,
+					"description" => __("The spacing for each item in the carousel.", "gears")
+				),
+				array(
+					"type" => "textfield",
+					"holder" => "",
+					"class" => "",
+					"admin_label" => true,
+					"admin_label" => true,
+					"heading" => __("Item Width", "gears"),
+					"param_name" => "item_width",
+					"value" => 228,
+					"description" => sprintf(__("The width of each item in the slide. Recommended maximum width is '%s'", "gears"), $bp_avatar_full_width)
+				)
+			)
+	)
+);
 ?>
