b169a4f68506a914567ff76dcb8d9bca09ffa2ee
Joseph Gabito <codehaiku.io@gmail.com>
2019-06-24 16:41:11 +0800

    updated upgrade checker

--- update-check.php
+++ update-check.php
@@ -1,129 +1,34 @@
 <?php
 /**
- * Auto update file
+ * Plugin Update Checker Library 4.6
+ * http://w-shadow.com/
  *
- * Enables auto update mode for gears plugin
- *
- * PHP version 5.6
- *
- * LICENSE: This source file is subject to version 3.01 of the PHP license
- * that is available through the world-wide-web at the following URI:
- * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
- * the PHP License and are unable to obtain it through the web, please
- * send a note to license@php.net so we can mail you a copy immediately.
- *
- * @category   Gears
- * @package    Klein
- * @author     dunhakdis 
- * @copyright  dunhakdis
- * @license    GPL2
- * @version    1.0
- * @since      1.0
+ * Copyright 2019 Janis Elsts
+ * Released under the MIT license. See license.txt for details.
  */
 
-/**
- * The public api url where the file is accessible and downloadable
- * @var string
- */
-$api_url = 'http://107.170.94.164/plugins/updates/api/index.php';
+require dirname(__FILE__) . '/Puc/v4p6/Factory.php';
+require dirname(__FILE__) . '/Puc/v4/Factory.php';
+require dirname(__FILE__) . '/Puc/v4p6/Autoloader.php';
+new Puc_v4p6_Autoloader();
 
-$plugin_slug = basename(dirname(__FILE__));
+//Register classes defined in this version with the factory.
+foreach (
+	array(
+		'Plugin_UpdateChecker' => 'Puc_v4p6_Plugin_UpdateChecker',
+		'Theme_UpdateChecker'  => 'Puc_v4p6_Theme_UpdateChecker',
 
-if ( GEARS_IS_UPDATE_DEBUG_MODE ) 
-{
-	
-	set_site_transient('update_plugins', null);
+		'Vcs_PluginUpdateChecker' => 'Puc_v4p6_Vcs_PluginUpdateChecker',
+		'Vcs_ThemeUpdateChecker'  => 'Puc_v4p6_Vcs_ThemeUpdateChecker',
 
-	add_filter('plugins_api_result', 'gears_debug_results', 10, 3);
-
-	function gears_debug_results($res, $action, $args) 
-	{	
-		var_dump($res);
-			return $res;
-	}
+		'GitHubApi'    => 'Puc_v4p6_Vcs_GitHubApi',
+		'BitBucketApi' => 'Puc_v4p6_Vcs_BitBucketApi',
+		'GitLabApi'    => 'Puc_v4p6_Vcs_GitLabApi',
+	)
+	as $pucGeneralClass => $pucVersionedClass
+) {
+	Puc_v4_Factory::addVersion($pucGeneralClass, $pucVersionedClass, '4.6');
+	//Also add it to the minor-version factory in case the major-version factory
+	//was already defined by another, older version of the update checker.
+	Puc_v4p6_Factory::addVersion($pucGeneralClass, $pucVersionedClass, '4.6');
 }
-
-
-/**
- * Take over the update check
- */
-add_filter('pre_set_site_transient_update_plugins', 'check_for_plugin_update');
-
-function check_for_plugin_update($checked_data) {
-
-	global $api_url, $plugin_slug, $wp_version;
-	
-	/**
-	 * Check if there are updates available
-	 */
-	if (!GEARS_IS_UPDATE_DEBUG_MODE) {
-		if (empty($checked_data->checked))
-			return $checked_data;
-	}
-	
-	$args = array(
-		'slug' => $plugin_slug,
-		'version' => $checked_data->checked[$plugin_slug .'/'. $plugin_slug .'.php'],
-	);
-	
-	$request_string = array(
-			'body' => array(
-				'action' => 'basic_check', 
-				'request' => serialize($args),
-				'api-key' => md5(get_bloginfo('url'))
-			),
-			'user-agent' => 'WordPress/' . $wp_version . '; ' . get_bloginfo('url')
-		);
-	
-	// Start checking for an update
-	$raw_response = wp_remote_post($api_url, $request_string);
-	
-	$response = "";
-	
-	if (!is_wp_error($raw_response) && ($raw_response['response']['code'] == 200))
-		$response = unserialize($raw_response['body']);
-	
-	if (is_object($response) && !empty($response)) // Feed the update data into WP updater
-		$checked_data->response[$plugin_slug .'/'. $plugin_slug .'.php'] = $response;
-	
-	return $checked_data;
-}
-
-
-// Take over the Plugin info screen
-add_filter('plugins_api', 'plugin_api_call', 10, 3);
-
-function plugin_api_call($def, $action, $args) {
-	global $plugin_slug, $api_url, $wp_version;
-	
-	if (!isset($args->slug) || ($args->slug != $plugin_slug))
-		return false;
-	
-	// Get the current version
-	$plugin_info = get_site_transient('update_plugins');
-	$current_version = $plugin_info->checked[$plugin_slug .'/'. $plugin_slug .'.php'];
-	$args->version = $current_version;
-	
-	$request_string = array(
-			'body' => array(
-				'action' => $action, 
-				'request' => serialize($args),
-				'api-key' => md5(get_bloginfo('url'))
-			),
-			'user-agent' => 'WordPress/' . $wp_version . '; ' . get_bloginfo('url')
-		);
-	
-	$request = wp_remote_post($api_url, $request_string);
-	
-	if (is_wp_error($request)) {
-		$res = new WP_Error('plugins_api_failed', __('An Unexpected HTTP Error occurred during the API request.</p> <p><a href="?" onclick="document.location.reload(); return false;">Try again</a>'), $request->get_error_message());
-	} else {
-		$res = unserialize($request['body']);
-		
-		if ($res === false)
-			$res = new WP_Error('plugins_api_failed', __('An unknown error occurred'), $request['body']);
-	}
-	
-	return $res;
-}
-?>
